f075b4db0702977a5c5946830c7d8f937a76b588
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-06-27 01:43:19 +0900

    upgrade

--- bootstrap/global.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * global.php
- * for dependency injection, service locator etc..
- */
-$app = new \Comnect\Console\Controller;
-
-return $app;
--- src/config/database.php
+++ src/config/database.php
@@ -4,33 +4,42 @@
  * supports four database systems: MySQL, Postgres, SQLite, and SQL Server.
  */
 return array(
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Database Connections
-	|--------------------------------------------------------------------------
-	*/
-	'connections' => array(
-		'master' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'username',
-			'password'  => 'password',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_general_ci',
-			'prefix'    => '',
-		),
 
-		'slave' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'username',
-			'password'  => 'password',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_general_ci',
-			'prefix'    => '',
-		),
-	)
+    /*
+    |--------------------------------------------------------------------------
+    | PDO Fetch Style
+    |--------------------------------------------------------------------------
+    */
+
+    'fetch' => PDO::FETCH_CLASS,
+
+    'default' => 'mysql',
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    */
+    'connections' => array(
+        'master' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database',
+            'username'  => 'username',
+            'password'  => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => '',
+        ),
+
+        'slave' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database',
+            'username'  => 'username',
+            'password'  => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => '',
+        ),
+    )
 );
